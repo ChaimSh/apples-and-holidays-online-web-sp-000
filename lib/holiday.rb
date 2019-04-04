b411c76964faccb4cbbaa16bbb1f4d4401255c11
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-04-04 03:04:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}:"
      holiday.each do |holiday, supplies|
-       puts "  #{holiday.to_s.capitalize.split("_").join(" ")}: #{supplies.join(", ")}"
+       puts "  #{holiday.split("_").join(" ").capitalize}: #{supplies.join(", ")}"
      end
      end
 end
