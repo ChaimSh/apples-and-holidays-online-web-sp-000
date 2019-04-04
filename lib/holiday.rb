36cb39fc408b872af376c6a2fa4c4fd00bc1f0ae
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-04-04 03:17:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,9 +68,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.collect do |key, value|
+holiday_hash.each do |key, value|
+new_array = []
   if value == "BBQ"
-  return  key
+  new_array << key
   end
 end
+new_array
 end
