6b083e655470abb4788d054330870e88dddcf9f2
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-04-04 02:45:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}:"
      holiday.each do |holiday, supplies|
-       puts "  #{holiday.to_s.capitalize}: #{supplies.to_s.capitalize.flatten}"
+       puts "  #{holiday.to_s.capitalize}: #{supplies.to_s.capitalize}"
      end
      end
 end
